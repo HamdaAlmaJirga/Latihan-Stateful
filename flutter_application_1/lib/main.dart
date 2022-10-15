94ce5222bdadc8ad37522694cf03eb658fbdba0c
Akbarfauzy454 <akbarfauzy54@gmail.com>
2022-10-15 12:50:38 +0700

    Latihan Stateful

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // disini bisa buat method
+  int number = 1;
+  void tekanTombol(){
+    setState(() {
+      number = number +1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan StatefulWidget"),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              MaterialButton(
+                color: Colors.blue[100],
+                height: 50,
+                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(5.0) ),
+                onPressed: tekanTombol,
+                child: Text("Tambah"),
+                )
+            ],
+          ),
+        ),
+         ),
+    );
+  }
+}
